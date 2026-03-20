ca0533097b99311bfb4d90826b87b6fd7d86f7f5
AS <asafshmueli@gmail.com>
2026-03-20 18:27:33 +0200

    broke down business logic to small util functions, none checks for consistency

--- log.py
+++ log.py
@@ -1,6 +1,38 @@
 from datetime import datetime
 from dal import logs, new_unique_id
 
+def _check_size(size):
+    """
+    Checks the size of the number 2 and returns a message 
+    based on the size compared to the worldwide average.    
+    """
+    if 2 <= size <= 4:
+        message = "\n\nYour number 2 diameter is within the worldwide average"
+    elif size > 4:
+        message = "\n\nYour number 2 diameter is bigger then worldwide average, well done!"
+    elif size < 2 and size != 0:
+        message = "\n\nYour number 2 diameter is smaller then worldwide average, ):"
+
+    return message
+
+def _ask_for_size():
+    """
+    Asks the user for the size of their number 2 and checks it against the worldwide average.
+    """
+    size = 0
+
+    raw_size = input("Type 2 selected, please enter the diameter of your type 2 in cm (optional): ")
+
+    if raw_size == "":
+        size = 0
+    else:
+        try:
+            size = int(raw_size)
+        except ValueError:
+            size = 0
+
+    return _check_size(size)
+
 def create_log_entry():
     """
     Creates a new log entry.
@@ -15,36 +47,23 @@ def create_log_entry():
 
     # added a small check if the user selected number 2, if they did ask them 
     # for number 2 size, and display a message if the size is within the worldwide average or not
-    size = 0
+    message = None
+
     if type == "2":
-       raw_size = input("Type 2 selected, please enter the diameter of your type 2 in cm (optional): ")
-       if raw_size == "":
-           size=0
-       else:
-           try:
-               size = int(raw_size)
-           except ValueError:
-               size = 0
-    
+        message =_ask_for_size()
+
     notes = input("Enter any notes (optional): ")
 
-    # printing messages after enter any notes for consisitency 
-    if 2 <= size <= 4:
-        print ("\n\nYour number 2 diameter is within the worldwide average")
-    elif size > 4:
-        print ("\n\nYour number 2 diameter is bigger then worldwide average, well done!")
-    elif size < 2 and size != 0:
-        print ("\n\nYour number 2 diameter is smaller then worldwide average, ):")
-    
+    # printing message after enter any notes for consistency 
+    if message is not None:
+        print(message)
 
-    entry = {
+    return {
         "id": new_unique_id(),
         "type": type,
         "notes": notes,
         "timestamp": str(datetime.now())
     }
-
-    return entry
 
 def log_entry():
     """
